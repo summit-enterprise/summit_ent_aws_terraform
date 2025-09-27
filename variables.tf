39228263fc8158e0563bd1c0a2ea211c30f54442
Moon Ahmed <admin@ma-summit-enterprise.com>
2025-09-27 15:30:40 -0500

    variables test

--- variables.tf
+++ variables.tf
@@ -1,5 +1,11 @@
 variable "AWS_DEFAULT_REGION" {
-    description ="AWS default region"
+    description = "AWS default region"
     type = string
     default = "us-east-2"
+}
+
+variable "TEST_VAR" {
+    description = "A test variable with no default value"
+    type = string
+    default = "my-custom-value"
 }
